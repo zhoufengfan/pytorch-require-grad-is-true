8a1e1266acb9cd4d84bed49527b189d752b3b196
zhoufengfan <zhfengfan@126.com>
2020-12-26 19:25:33 +0800

    Add mse loss

--- tensor_require_grad_is_true.py
+++ tensor_require_grad_is_true.py
@@ -2,18 +2,17 @@ import torch
 import torch.nn as nn
 
 if __name__ == '__main__':
-    epoch = 10000
+    epoch = 5000
     # It's better not to use `reshape` to change the shape of the tensor. Because it'll will change the tensor to
     # non-leaf.
     t1 = torch.tensor([[20.0, 1.0, 30.0]], requires_grad=True)
     t2 = torch.tensor([[1.0, 2.0, 1.0]], requires_grad=True)
     l3 = [t1]
     optimizer = torch.optim.Adam(l3, lr=0.01)
-    # criterion = nn.KLDivLoss(reduction="batchmean")
-    criterion = nn.MSELoss()
+    criterion = nn.KLDivLoss(reduction="batchmean")
     for i in range(epoch):
-        # loss = criterion(torch.log(t2), t1)
-        loss = criterion(t2, t1)
+        loss = criterion(torch.log(t2), t1)
+        # loss = criterion(t2, t1)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
