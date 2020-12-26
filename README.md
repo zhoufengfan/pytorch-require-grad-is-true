c28563fef3f33a0c50c254697c2d752ad60cfbcc
zhoufengfan <zhfengfan@126.com>
2020-12-26 19:23:05 +0800

    change to mseloss

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Test of PyTorch tensor that require_grad is True.
 
 The code also test the effect of kldivloss. It shows that kldivloss can make the distribution of 2 tensor closer.
+
+In the `mseloss`  branch, the code show that `mseloss` can make 2 tensor the same.
--- tensor_require_grad_is_true.py
+++ tensor_require_grad_is_true.py
@@ -2,16 +2,18 @@ import torch
 import torch.nn as nn
 
 if __name__ == '__main__':
-    epoch = 5000
+    epoch = 10000
     # It's better not to use `reshape` to change the shape of the tensor. Because it'll will change the tensor to
     # non-leaf.
     t1 = torch.tensor([[20.0, 1.0, 30.0]], requires_grad=True)
-    t2 = torch.tensor([[1.0, 1.0, 1.0]])
+    t2 = torch.tensor([[1.0, 2.0, 1.0]], requires_grad=True)
     l3 = [t1]
     optimizer = torch.optim.Adam(l3, lr=0.01)
-    criterion = nn.KLDivLoss(reduction="batchmean")
+    # criterion = nn.KLDivLoss(reduction="batchmean")
+    criterion = nn.MSELoss()
     for i in range(epoch):
-        loss = criterion(torch.log(t2), t1)
+        # loss = criterion(torch.log(t2), t1)
+        loss = criterion(t2, t1)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
